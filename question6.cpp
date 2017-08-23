9aa6607d44c26b2ee7901621c3d206b0eadd4951
RiteshDalai <31160848+RiteshDalai@users.noreply.github.com>
2017-08-23 19:12:08 +0000

    Update question6.cpp

--- question6.cpp
+++ question6.cpp
@@ -8,6 +8,8 @@ double circle()
 	cin >> r;
 	double d=2*r;
 	cout << "diamater of the circle is : " << d <<endl;
+	double c=(2*3.14*r);
+	cout << "circumference of the circle is : " << c << endl; 
 	double a=3.14*(r*r);
 	cout << "The area of the circle is : " << a << endl;
 }
